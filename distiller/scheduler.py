f08ee463d84322c5a13a327a6c1143f78e97d36d
lorenzfe <felixlorenz@posteo.de>
2019-08-16 10:34:20 +0200

    prints

--- distiller/scheduler.py
+++ distiller/scheduler.py
@@ -54,7 +54,9 @@ class ParameterMasker(object):
     def mask_tensor(self, tensor):
         if self.mask is not None:
             print(self.mask.size())
+            print(self.mask)
             print(tensor.size())
+            print(tensor)
             tensor.data.mul_(self.mask)
 
     def mask_gradient(self, gradient):
